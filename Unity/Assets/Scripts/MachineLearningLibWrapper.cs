aaa446a540065b50881a5abdec3faff861f6327f
Ismaïl <nguyen.ismail@gmail.com>
2017-03-17 11:29:27 +0100

    separate folder for unity scenes, and dll wrapper

---
+++ Unity/Assets/Scripts/MachineLearningLibWrapper.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class MachineLearningLibWrapper {
+
+	public static class Linear {
+		
+		[DllImport("LinearModelLib")]
+		public static extern System.IntPtr linear_create_model(int inputDimension);
+
+		[DllImport("LinearModelLib")]
+		public extern void linear_remove_model(System.IntPtr model);
+
+		[DllImport("LinearModelLib")]
+		public static extern int linear_fit_regression(System.IntPtr model, System.IntPtr inputs, int inputsSize, int inputSize, System.IntPtr outputs, int outputsSize);
+
+		[DllImport("LinearModelLib")]
+		public static extern int linear_fit_classification_rosenblatt(System.IntPtr model, System.IntPtr inputs, int inputsSize, int inputSize, int iterationNumber, double step, System.IntPtr outputs);
+		
+		[DllImport("LinearModelLib")]
+		public static extern double linear_classify(System.IntPtr model, System.IntPtr input, int inputSize);
+		
+		[DllImport("LinearModelLib")]
+		public static extern double linear_predict(System.IntPtr model, System.IntPtr input, int inputSize);
+	}
+
+	public static class MLP {
+
+		[DllImport("MLPLib")]
+		public static extern System.IntPtr[] mlp_create_model(int numLayers, System.IntPtr nlp);
+		
+		[DllImport("MLPLib")]
+		public static extern void mlp_remove_model(MLP * model);
+		
+		[DllImport("MLPLib")]
+		public static extern int mlp_fit_regression_backdrop(System.IntPtr[] model, System.IntPtr inputs, int inputsSize, int inputSize, System.IntPtr outputs, int outputsSize, int outpoutSize, int iterationNumber, double step);
+		
+		[DllImport("MLPLib")]
+		public static extern int mlp_fit_classification_backdrop(System.IntPtr[] model, System.IntPtr inputs, int inputsSize, int inputSize, System.IntPtr outputs, int outputsSize, int outpoutSize, int iterationNumber, double step);
+		
+		[DllImport("MLPLib")]
+		public static extern System.IntPtr mlp_classify(System.IntPtr[] model, System.IntPtr input, int inputSize);
+		
+		[DllImport("MLPLib")]
+		public static extern System.IntPtr mlp_predict(System.IntPtr[] model, System.IntPtr input, int inputSize); 
+	}
+
+	public static class RBF {
+
+		//[DllImport("RBFLib")]
+	}
+}
---
+++ Unity/Assets/Scripts/TestLinearModel.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestLinearModel : MonoBehaviour {
+
+	[SerializeField]
+	private Transform[] spheresTransform;
+
+	// Modèle
+	private System.IntPtr model;
+
+	// Use this for initialization
+	void Start () {
+		// Initialisation du modèle
+		model = MachineLearningLibWrapper.linear_create_model(2); // 2 dimensions
+
+		foreach (var sphere in spheresTransform) {
+			sphere.position += Vector3.up * 2;
+
+			//MachineLearningLibWrapper.linear_fit_classification_rosenblatt(model, inputs, inputsSize, inputSize, iterationNumber, step, outputs);
+
+			MachineLearningLibWrapper.linear_classify( model, input, inputSize);
+		}
+
+		MachineLearningLibWrapper.linear_remove_model(model);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
